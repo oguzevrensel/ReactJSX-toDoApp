66dc89dcab9f8467a9ac2c3ad503d335c6e50605
Oğuz Evrensel <oevrensel@gmail.com>
2022-06-23 22:15:40 +0300

    Create Search.js

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {MdSearch} from "react-icons/md"
+
+
+function Search({handleSearch}) {
+    return (
+
+        <div className='search'>
+
+        <MdSearch className="search-icon" size="1.4em"/>
+
+        <input type="text" placeholder='Arama Yap' onChange={(event)=> handleSearch(event.target.value)}/>
+        </div>
+    )
+}
+
+export default Search
